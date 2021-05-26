783566da2bc415b9ebe9aae46b09f59614a0ffd9
nomanme <abdullah.noman083@gmail.com>
2021-05-26 21:27:04 +0600

    added

--- 12-Getting-started-with-JavaScript/hello.js
+++ 12-Getting-started-with-JavaScript/hello.js
@@ -1,3 +1,8 @@
+// 12-2 Run JavaScript in browser, VSCode, and codepen
 console.log(21);
 
 console.log("Good Morning!");
+
+// 12-3 What is variable, declare a variable, see output
+var bananaPrice = 16;
+console.log(bananaPrice);
