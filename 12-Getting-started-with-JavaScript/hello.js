f238ffeeb98ddbf2c478d2d35df4a44baa27624d
nomanme <abdullah.noman083@gmail.com>
2021-05-26 21:40:07 +0600

    var type added

--- 12-Getting-started-with-JavaScript/hello.js
+++ 12-Getting-started-with-JavaScript/hello.js
@@ -6,3 +6,14 @@ console.log("Good Morning!");
 // 12-3 What is variable, declare a variable, see output
 var bananaPrice = 16;
 console.log(bananaPrice);
+
+//  12-4 Variable type, Numeric, String, Boolean
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var uname = "kalimuddin zuckerburg";
+console.log(typeof uname);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
