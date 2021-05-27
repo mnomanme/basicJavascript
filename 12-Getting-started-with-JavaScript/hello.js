25c87e6b9b7dc32115665843c7c06c0ae8b66b17
nomanme <abdullah.noman083@gmail.com>
2021-05-27 23:06:05 +0600

    conversion type added

--- 12-Getting-started-with-JavaScript/hello.js
+++ 12-Getting-started-with-JavaScript/hello.js
@@ -31,3 +31,25 @@ console.log(promise.toLowerCase());
 console.log(promise.toUpperCase());
 console.log(promise.indexOf("HARD"));
 console.log(promise.split(" "));
+
+// 12-7 Integer float parseInt parseFloat type conversion
+var number1 = 20;
+var number2 = 20.5;
+console.log(number1 + number2);
+
+var number3 = 33;
+var number4 = "17.5";
+number4 = parseFloat(number4); // convert floating number
+number4 = parseInt(number4); // convert integer number
+console.log(number3 + number4);
+
+var number5 = 30;
+var number6 = 40.5;
+number5 = "" + number5; // convert string
+console.log(typeof number5);
+
+var number7 = 0.1;
+var number8 = 0.2;
+var total = number7 + number8;
+total = total.toFixed(2);
+console.log(total);
