6703db43d890c6d65da19319fb529755a26dcc14
nomanme <abdullah.noman083@gmail.com>
2021-05-31 22:32:03 +0600

    array add

---
+++ 12-Getting-started-with-JavaScript/array.js
@@ -0,0 +1,10 @@
+// 13-2 Array, index, set by index, indexOf
+var friendsAge = [12, 15, 18, 21];
+
+var sonaliAge = friendsAge[2];
+
+friendsAge[1] = 50; // set array index
+
+var position = friendsAge.indexOf(201); // find array index
+
+console.log(position);
