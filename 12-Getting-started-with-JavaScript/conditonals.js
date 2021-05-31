e6abcc9eaee78c92e2c26bbd76fb83c772e9b852
nomanme <abdullah.noman083@gmail.com>
2021-05-31 14:49:10 +0600

    conditals added

---
+++ 12-Getting-started-with-JavaScript/conditonals.js
@@ -0,0 +1,28 @@
+// 12-10 Make conditional decision, if-else, comparison
+
+var biscuitPrice = 8;
+
+if (biscuitPrice < 10) {
+  // '<' this is less than sign
+  console.log("I will eat the biscuits");
+} else {
+  console.log("Khali cha is better");
+}
+
+var busFare = 1000;
+
+if (busFare == 1000) {
+  // '==' this is equal sign
+  console.log("I will go");
+} else {
+  console.log("train is better");
+}
+
+var busFare = 1000;
+
+if (busFare != 1000) {
+  // '==' this is not equal sign
+  console.log("I will go");
+} else {
+  console.log("train is better");
+}
