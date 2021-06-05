d3a2e32103c7962dadfd0e0a6473236c5bd989db
nomanme <abdullah.noman083@gmail.com>
2021-06-05 20:07:02 +0600

    for loop array add

--- 13-Javascript-fundamental-concepts/forLoop.js
+++ 13-Javascript-fundamental-concepts/forLoop.js
@@ -1,1 +1,19 @@
 //13-6 For loop loop, run a loop for each element of an array
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var nums = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+
+for (var i = 0; i < nums.length; i++) {
+  console.log(i);
+  var element = nums[i];
+  console.log(element);
+}
+
+var friends = ["Montu", "Sezan", "Naeem", "Noman"];
+
+for (i = 0; i < friends.length; i++) {
+  var friendsName = friends[i];
+  console.log(friendsName);
+}
