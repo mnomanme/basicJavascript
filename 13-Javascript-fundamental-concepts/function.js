0970713979f28c4d630f606165258ade7de34d6a
nomanme <abdullah.noman083@gmail.com>
2021-07-07 20:14:31 +0600

    sum of two value

--- 13-Javascript-fundamental-concepts/function.js
+++ 13-Javascript-fundamental-concepts/function.js
@@ -13,3 +13,33 @@ sayLoveYou();
 var hangoutPlace = 'TSC';
 
 sayLoveYou();
+
+// 13-9 Function Parameter, multiple parameter, function return
+function doubleIt(num) {
+	var result = num * 2;
+	console.log(result);
+}
+doubleIt(5);
+doubleIt(50);
+doubleIt(500);
+
+function doubleNum(num) {
+	var result = num * 2;
+	return result;
+}
+
+var first = doubleNum(5);
+var second = doubleNum(50);
+var third = doubleNum(500);
+
+var total = first + second + third;
+
+console.log(first, second, third);
+console.log(total);
+
+function add(num1, num2) {
+	var result = num1 + num2;
+	return result;
+}
+var sum = add(15, 25);
+console.log(sum);
