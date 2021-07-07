afbe75a7e4ab60b2bf02e0e95d48faf2aa005f34
nomanme <abdullah.noman083@gmail.com>
2021-07-07 15:04:06 +0600

    function call & declare

---
+++ 13-Javascript-fundamental-concepts/function.js
@@ -0,0 +1,15 @@
+// 13-8 Function, call function
+function sayLoveYou() {
+	console.log('Jaan o baby');
+	console.log('Billi baby... koi tumi');
+}
+sayLoveYou();
+
+var date = 14;
+var place = 'Restaurant';
+
+sayLoveYou();
+
+var hangoutPlace = 'TSC';
+
+sayLoveYou();
