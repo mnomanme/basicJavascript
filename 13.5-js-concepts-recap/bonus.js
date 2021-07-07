fc7195c842ec2c1dfc041624b58c50adcfef5ef3
nomanme <abdullah.noman083@gmail.com>
2021-07-07 23:50:04 +0600

    loop

---
+++ 13.5-js-concepts-recap/bonus.js
@@ -0,0 +1,9 @@
+//13_5-1 What is javascript, key features of javascript
+// 13_5-3 Revisit string, array, loop
+
+var marks = [66, 99, 70, 77, 88, 40, 39];
+
+for (i = 0; i < marks.length; i++) {
+	var elements = marks[i];
+	console.log(elements);
+}
