28aaa11e3b80c06a6390bd90b941a8fb79b29dc2
nomanme <abdullah.noman083@gmail.com>
2021-07-08 11:54:44 +0600

    let & const

---
+++ 14-apply-javaScript-concepts/constLet.js
@@ -0,0 +1,17 @@
+//14-3 variable let and const and how to use them
+var userName = 'Noman';
+console.log(userName.length);
+
+if (userName.length > 4) {
+	userName = 'Mohammad';
+}
+console.log(userName);
+
+let age = 12; // let variable is used to be changeable to let value
+age = 13;
+age = 14;
+
+console.log(age);
+
+const country = 'Bangladesh'; // const variable is used to be fixed varibale
+console.log(country);
