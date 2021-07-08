28b4835e7daed2ca7ac07a4f7354519ecf6f1e53
nomanme <abdullah.noman083@gmail.com>
2021-07-08 11:30:04 +0600

    convert inch to feet

---
+++ 14-apply-javaScript-concepts/inchToFeet.js
@@ -0,0 +1,21 @@
+//14-2 Unit Convert Inch to Feet use variable and function
+var inch = 156;
+var feet = inch / 12;
+
+console.log(feet);
+console.log(feet);
+console.log(feet);
+
+function inchToFeet(inch) {
+	var feet = inch / 12;
+	return feet;
+}
+
+var nanaFeet = inchToFeet(120);
+console.log(nanaFeet);
+
+var naniFeet = inchToFeet(144);
+console.log(naniFeet);
+
+var dadiFeet = inchToFeet(180);
+console.log(dadiFeet);
