c3bce0e70447f0f565bd88ad13841538509efbfa
nomanme <abdullah.noman083@gmail.com>
2021-07-10 12:00:47 +0600

    :sum of array

---
+++ 15- javaScript-coding-problems-simple-interview-questions/arraySum.js
@@ -0,0 +1,24 @@
+//15-6 Sum of all numbers in an array
+
+var numbers = [56, 37, 29, 20, 44, 90, 66];
+var sum = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+	var element = numbers[i];
+	sum = sum + element;
+}
+console.log('Sum of:', sum);
+
+// sum of array using function
+
+function getArrayMarks(marks) {
+	var sum = 0;
+	for (var i = 0; i < marks.length; i++) {
+		var element = marks[i];
+		sum = sum + element;
+	}
+	return sum;
+}
+var marksExam = [47, 29, 42, 90];
+var total = getArrayMarks(marksExam);
+console.log('Exam marks:', total);
