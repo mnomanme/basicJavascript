620895d247c8c6bef7c8a2da8a980ef5deb6f9da
nomanme <abdullah.noman083@gmail.com>
2021-07-11 01:08:25 +0600

    recursion done

--- 15.5-practice-basic-javaScript/recursion.js
+++ 15.5-practice-basic-javaScript/recursion.js
@@ -4,9 +4,26 @@ function factorial(num) {
 	var fact = 1;
 	for (var i = 1; i <= num; i++) {
 		fact = fact * i;
-		// console.log(i, fact);
+		console.log('i:', i, 'fact:', fact);
 	}
 	return fact;
 }
 var result = factorial(5);
 console.log('fact result:', result);
+
+// recursive factorial
+// 5! = 5*4*3*2*1
+// factorialRecursive(num - 1)
+
+function factorialRecursive(num) {
+	if (num == 1) {
+		console.log('1st:', num);
+		return 1;
+	} else {
+		console.log('2nd', num, '3rd:', num - 1);
+		return num * factorialRecursive(num - 1);
+	}
+}
+
+var numFact = factorialRecursive(6);
+console.log('result:', numFact);
