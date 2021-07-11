1ca919c0730efa49901287c5e7cfb83498f41e42
nomanme <abdullah.noman083@gmail.com>
2021-07-11 12:21:04 +0600

    wood calculator

--- 16-assignment/assignment01.js
+++ 16-assignment/assignment01.js
@@ -13,3 +13,15 @@ let result = feetToMile(90909);
 result = result.toFixed(2);
 
 console.log('Feet to Mile = >', result);
+
+// Problem two solve
+
+// woodCalculator
+function woodCalculator(chair, table, cot) {
+	var furniture = 1 * chair + 3 * table + 5 * cot;
+	return furniture;
+}
+
+let price = woodCalculator(2, 3, 4);
+
+console.log('My furniture cost = >', price);
