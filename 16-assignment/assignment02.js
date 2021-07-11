77ed1e31fa5bbd3cb62596e9f048033c1c6f28f8
nomanme <abdullah.noman083@gmail.com>
2021-07-11 13:04:57 +0600

    meter to kilometer

---
+++ 16-assignment/assignment02.js
@@ -0,0 +1,11 @@
+// problem 01 solve
+
+// kilometerToMeter
+
+function kilometerToMeter(meter) {
+	var km = meter * 1000; // 1 km = 1000 meter
+	console.log('Check kilometer =>', km);
+	return km;
+}
+var kMeter = kilometerToMeter(1);
+console.log('Here KiloMeter is =>', kMeter);
