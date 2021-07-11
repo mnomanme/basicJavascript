3d1da0499d06fe3662a7c7a66fc6bd55e135a98d
nomanme <abdullah.noman083@gmail.com>
2021-07-11 11:44:49 +0600

    counting animals

--- 15.5-practice-basic-javaScript/animal.js
+++ 15.5-practice-basic-javaScript/animal.js
@@ -1,6 +1,6 @@
 // 15_5-2 Travelling in a Jungle and counting wild animals
 
-var depth = 13;
+var depth = 30;
 var animal = 0;
 
 if (depth <= 10) {
@@ -9,7 +9,8 @@ if (depth <= 10) {
 } else if (depth <= 20) {
 	var firstPart = 10 * 50;
 	var remaining = depth - 10;
-	console.log(remaining);
+	console.log('remaind', remaining);
+
 	var secondPart = remaining * 100;
 	// console.log(secondPart);
 	animal = firstPart + secondPart;
@@ -18,9 +19,44 @@ if (depth <= 10) {
 	var firstPart = 10 * 50;
 	var secondPart = 10 * 100;
 	var remaining = depth - 20;
+	console.log('remaind', remaining);
+
 	var thirdPart = remaining * 300;
 	// console.log(thirdPart);
 	animal = firstPart + secondPart + thirdPart;
 	console.log('third:', animal);
 }
 console.log('Total animal =>', animal);
+
+// animal count using function
+
+function animalCount(depth) {
+	var animal = 0;
+
+	if (depth <= 10) {
+		animal = depth * 50;
+		console.log('first:', animal);
+	} else if (depth <= 20) {
+		var firstPart = 10 * 50;
+		var remaining = depth - 10;
+		console.log('remaind', remaining);
+
+		var secondPart = remaining * 100;
+		// console.log(secondPart);
+		animal = firstPart + secondPart;
+		console.log('second:', animal);
+	} else {
+		var firstPart = 10 * 50;
+		var secondPart = 10 * 100;
+		var remaining = depth - 20;
+		// console.log('remaind', remaining);
+
+		var thirdPart = remaining * 300;
+		// console.log(thirdPart);
+		animal = firstPart + secondPart + thirdPart;
+		// console.log('third:', animal);
+	}
+	return animal;
+}
+var animal = animalCount(19);
+console.log('Jungle animal =>', animal);
