a749a29a83cf96a096a8d4dbf84c82a9f07cb215
nomanme <abdullah.noman083@gmail.com>
2021-07-11 14:04:00 +0600

    tour cost

--- 16-assignment/assignment02.js
+++ 16-assignment/assignment02.js
@@ -9,3 +9,34 @@ function kilometerToMeter(meter) {
 }
 let kMeter = kilometerToMeter(18.5);
 console.log('Here KiloMeter is =>', kMeter);
+
+// problem 02 solve
+
+// budgetCalculator
+
+function budgetCalculator(watch, phone, laptop) {
+	let device = 3 * watch + 2 * phone + 1 * laptop;
+	return device;
+}
+let price = budgetCalculator(50, 100, 500);
+console.log('Total Price =>', price);
+
+// problem 03 solve
+
+// hotelCost
+
+function hotelCost(cost) {
+	let day = cost;
+	if (day <= 10) {
+		// console.log('first week cost:', day);
+		return day * 100;
+	} else if (day <= 20) {
+		// console.log('second week cost:', day);
+		return 10 * 100 + (day - 10) * 80;
+	} else {
+		// console.log('third week cost:', day);
+		return 10 * 100 + 10 * 80 + (day - 20) * 50;
+	}
+}
+let totalCost = hotelCost(250);
+console.log('All tour cost =>', totalCost);
