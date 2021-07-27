e9d0b363e40b383c86cab742bb965762af471684
nomanme <abdullah.noman083@gmail.com>
2021-07-27 23:05:12 +0600

    indentation improve

--- 16-assignment/assignment02.js
+++ 16-assignment/assignment02.js
@@ -1,9 +1,7 @@
 // https://github.com/nomanme/basic-javascript/blob/master/16-assignment/assignment02.js
 
 // problem 01 solve
-
 // kilometerToMeter
-
 function kilometerToMeter(kilometer) {
 	if (typeof kilometer !== 'number' || kilometer === undefined || kilometer === null) {
 		return 'Please Enter Your Valid Value';
@@ -14,13 +12,12 @@ function kilometerToMeter(kilometer) {
 	// console.log('Check kilometer =>', meter);
 	return meter;
 }
+
 let totalKilometer = kilometerToMeter(18.5);
 console.log('Here KiloMeter is =>', totalKilometer);
 
 // problem 02 solve
-
 // budgetCalculator
-
 function budgetCalculator(watch, phone, laptop) {
 	if (typeof watch !== 'number' || watch === undefined || watch === null || watch < 0) {
 		return 'Please Enter Valid Number';
@@ -33,13 +30,12 @@ function budgetCalculator(watch, phone, laptop) {
 	let device = watch * 3 + phone * 2 + laptop * 1;
 	return device;
 }
+
 let price = budgetCalculator(50, 100, 500);
 console.log('Total Price =>', price);
 
 // problem 03 solve
-
 // hotelCost
-
 function hotelCost(days) {
 	let costs = days;
 	if (costs < 0 || costs % 1 !== 0 || costs === 0 || typeof costs === 'boolean') {
@@ -55,13 +51,12 @@ function hotelCost(days) {
 		return 10 * 100 + 10 * 80 + (costs - 20) * 50;
 	}
 }
+
 let totalCost = hotelCost(21);
 console.log('All tour cost =>', totalCost);
 
 // problem 04 solve
-
 // megaFriend
-
 let friendName = ['Mohammad', 'Noman', 'Faruque', 'Naeem', 'Zareen', 'Sultana', 'uyhgihujfdcuiyagbufbu'];
 
 function megaFriend(friend) {
